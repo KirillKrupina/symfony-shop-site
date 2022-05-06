c8a1d666ab9bbf8c6f71f32a6e3deac4d0de105d
Kirill Krupina <kirill.krupina26@gmail.com>
2022-05-06 14:50:35 +0300

    Example add/edit product form

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +17,7 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
+        // getDoctrine is deprecated
         $entityManager = $this->getDoctrine()->getManager();
         $productList = $entityManager->getRepository(Product::class)->findAll();
         dd($productList);
@@ -21,8 +27,10 @@ class DefaultController extends AbstractController
         ]);
     }
 
+    /*
+     * Test method
     #[Route('/product-add', name: 'product_add')]
-    public function productAt(): Response
+    public function productAdd(): Response
     {
         $product = new Product();
         $product->setTitle('Product '.rand(1, 100));
@@ -38,5 +46,43 @@ class DefaultController extends AbstractController
         $entityManager->flush();
 
         return $this->redirectToRoute('homepage');
+    }
+    */
+
+    #[Route('/edit-product/{id}', methods: ['GET', 'POST'], name: 'edit_product', requirements: ['id' => '\d+'])]
+    #[Route('/add-product', methods: ['GET','POST'], name: 'add_product')]
+    public function editProduct(Request $request, EntityManagerInterface $entityManager, int $id = null): Response
+    {
+        if (!$id) {
+            $product = new Product();
+        } else {
+            $product = $entityManager->getRepository(Product::class)->find($id);
+        }
+        $form = $this->createFormBuilder($product)
+            ->add('title', TextType::class)
+            ->add('price', NumberType::class)
+            ->add('quantity', IntegerType::class)
+            ->getForm();
+
+        /*  Receive request from form.
+            Takes the POST’ed data from the previous request, processes it,
+            and runs any validation (checks integrity of expected versus received data)
+        */
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Get data from form
+            $data = $form->getData();
+            // dd($data);
+            // Prepare object to insert
+            $entityManager->persist($product);
+            // Update DB
+            $entityManager->flush();
+
+            return $this->redirectToRoute('edit_product', ['id' => $product->getId()]);
+        }
+        return $this->render('main/default/edit_product.html.twig', [
+            'controller_name' => 'DefaultController',
+            'form' => $form->createView()
+        ]);
     }
 }
