2ab6dddd5e114bf6ccda62eba146f1c3a9eae079
Kirill Krupina <kirill.krupina26@gmail.com>
2022-05-19 14:10:54 +0300

    Added verified user validation in login form

--- src/Security/LoginFromAuthenticator.php
+++ src/Security/LoginFromAuthenticator.php
@@ -2,12 +2,16 @@
 
 namespace App\Security;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -23,10 +27,14 @@ class LoginFromAuthenticator extends AbstractLoginFormAuthenticator
     public const LOGIN_ROUTE = 'main_login';
 
     private UrlGeneratorInterface $urlGenerator;
+    private UserRepository $userRepository;
+    private FlashBagInterface $flashBag;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator)
+    public function __construct(UrlGeneratorInterface $urlGenerator, UserRepository $userRepository,  FlashBagInterface $flashBag)
     {
         $this->urlGenerator = $urlGenerator;
+        $this->userRepository = $userRepository;
+        $this->flashBag = $flashBag;
     }
 
     public function authenticate(Request $request): Passport
@@ -34,6 +42,11 @@ class LoginFromAuthenticator extends AbstractLoginFormAuthenticator
         $email = $request->request->get('email', '');
 
         $request->getSession()->set(Security::LAST_USERNAME, $email);
+
+        if (!$this->isUserVerified($request)) {
+            $this->flashBag->add('warning', 'Failed to login. Email is not verified.');
+            throw new AuthenticationException();
+        }
 
         return new Passport(
             new UserBadge($email),
@@ -43,6 +56,26 @@ class LoginFromAuthenticator extends AbstractLoginFormAuthenticator
                 new RememberMeBadge()
             ]
         );
+    }
+
+    public function isUserVerified(Request $request) {
+        $credentials = $this->getCredentials($request);
+        $user = $this->getUser($credentials);
+
+        return $user->isVerified();
+    }
+
+    public function getCredentials(Request $request)
+    {
+        return [
+            'email' => $request->request->get('email'),
+            'password' => $request->request->get('password'),
+        ];
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider = null)
+    {
+        return $this->userRepository->findOneBy(['email' => $credentials['email']]);
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
