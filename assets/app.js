6062096f1e535473041a63feca6f92e69002494d
Kirill Krupina <kirill.krupina26@gmail.com>
2022-05-23 13:29:59 +0300

    Created admin login page

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
+import './scripts/section-admin.js';
+
 // start the Stimulus application
 import './bootstrap';
