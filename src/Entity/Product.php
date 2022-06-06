03e20f077e3bf89c1c9c7a531c0b2786896eb016
Kirill Krupina <kirill.krupina26@gmail.com>
2022-06-06 15:45:44 +0300

    Added uuid to product entity

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: ProductRepository::class)]
 class Product
@@ -15,6 +16,9 @@ class Product
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private $id;
+
+    #[ORM\Column(type: 'uuid', nullable: false)]
+    private $uuid;
 
     #[ORM\Column(type: 'string', length: 255)]
     private $title;
@@ -46,6 +50,7 @@ class Product
 
     public function __construct()
     {
+        $this->uuid = Uuid::v4();
         $this->createdAt = new \DateTimeImmutable();
         $this->isPublished = false;
         $this->isDeleted = false;
@@ -55,6 +60,11 @@ class Product
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
     }
 
     public function getTitle(): ?string
